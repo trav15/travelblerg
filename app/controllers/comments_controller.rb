4f295f849bbc7f8983e3e6dd4c608a48561c9bc2
trav15 <travistokuyama@gmail.com>
2015-09-03 03:50:41 +0000

    Added delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,14 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
-      end
-     
+    end
+    
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+    end
     private
       def comment_params
         params.require(:comment).permit(:commenter, :body)
